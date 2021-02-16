6eb14c82fe8f646e562c54825fd1314bf7393d25
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-02-16 17:54:02 -0600

    PROPS - componentes dinamicos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import  Saludo from './components/Saludo.jsx';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1>Proyecto desde Cero</h1>
+      <Saludo persona='Diana' edad={24} />
+      <Saludo persona='Enrique' edad={25} />
+      <Saludo persona='Shellsea' edad={23} />
+      <Saludo persona='Omar' edad={23} />
     </div>
   );
 }
